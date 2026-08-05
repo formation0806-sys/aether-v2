335fb09868d6ec0d6ffd5d23846d66e1a98f6a16
formation0806-sys <formation0806@gmail.com>
2026-08-05 18:37:16 +0530

    Project foundation complete

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function SignUpPage() {
   return (
     <main
       style={{
@@ -10,7 +10,7 @@ export default function Home() {
         fontWeight: "bold",
       }}
     >
-      THIS IS AETHER 🚀
+      SIGN UP PAGE 🚀
     </main>
   );
 }
